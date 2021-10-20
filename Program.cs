079754c2e615d9a683b84567ded5984e5367e249
Adriano Maurmann <adriano.maurmann@ecs.com.br>
2021-10-20 11:36:22 -0300

    adicionado codigo lab

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace monad_maybe_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test1();
+            Test2();
+            Test3();
+        }
+
+        static void Test1()
+        {
+            var contents = GetLogContents(1);
+
+            if (contents is Maybe<string>.Some some)
+            {
+                Console.WriteLine(some.Value);
+            }
+            else
+            {
+                Console.WriteLine("Log file not found");
+            }
+        }
+
+        static void Test2()
+        {
+            var contents = GetLogContents(1);
+
+            if (contents.TryGetValue(out var value))
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("Log file not found");
+            }
+        }
+
+        static void Test3()
+        {
+            var contents = GetLogContents(1);
+
+            contents.Match(some: value =>
+            {
+                Console.WriteLine(value);
+            },
+            none: () =>
+            {
+                Console.WriteLine("Log file not found");
+            });
+        }
+
+        static Maybe<string> GetLogContents(int id)
+        {
+            var filename = "c:\\logs\\" + id + ".log";
+
+            if (File.Exists(filename))
+                return File.ReadAllText(filename);
+
+            return Maybe.None;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # maybe-monad
 Maybe Monad Pattern
+
+Article: https://www.dotnetcurry.com/patterns-practices/1510/maybe-monad-csharp
+
+Reference Code: https://github.com/ymassad/MaybeExamples/tree/master/MaybeAsASumType
